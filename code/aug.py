4e3f5ff55cecfb591459b05c0fe4cbd6bc6bf578
septiaindra <septian.wicaksana@icloud.com>
2023-06-25 13:08:51 +0700

    new augmented

--- code/aug.py
+++ code/aug.py
@@ -1,15 +1,15 @@
 import Augmentor
 
 # Specify the path to the input image
-input_image = "./raw_data/train/"
+input_image = f'C:/Users/septi/OneDrive/Documents/fsa/code/raw_data/train/dcom'
 
 # Create a pipeline object
 p = Augmentor.Pipeline(input_image)
 
 # Add image augmentation techniques
-p.rotate(probability=0.5, max_left_rotation=10, max_right_rotation=10)
-p.zoom(probability=0.5, min_factor=1.1, max_factor=1.5)
+p.rotate(probability=0.5, max_left_rotation=5, max_right_rotation=5)
+p.zoom(probability=0.5, min_factor=1.05, max_factor=1.08)
 p.flip_left_right(probability=0.5)
 
 # Generate new images
-p.sample(100)
+p.sample(40)
